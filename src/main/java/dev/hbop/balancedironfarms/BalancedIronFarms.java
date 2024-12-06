ba0cd93b8ee7c12be10f43b634b817bda6f11f08
HB0P <harrymilne2007@gmail.com>
2024-12-06 12:09:02 +0000

    correction

--- src/main/java/dev/hbop/balancedironfarms/BalancedIronFarms.java
+++ src/main/java/dev/hbop/balancedironfarms/BalancedIronFarms.java
@@ -5,7 +5,7 @@ import dev.hbop.balancedironfarms.ModConfig;
 
 public class BalancedIronFarms implements ModInitializer {
 
-    public static final String MOD_ID = "balancedstacksizes";
+    public static final String MOD_ID = "balancedironfarms";
     public static final ModConfig CONFIG = ModConfig.createAndLoad();
     
     @Override
